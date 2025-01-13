9791464d0783eaf84cf24e53c60dc8bfaea8a063
kamyar <54872856+noakishere@users.noreply.github.com>
2025-01-13 17:40:38 -0500

    btn fix

--- js/script.js
+++ js/script.js
@@ -14,7 +14,9 @@ loadRNBO();
 randomBtn.addEventListener("click", () => {
 	note = Math.floor(Math.random() * 20) + 50;
 	noteOn(device, context, note, 100);
+	randomBtn.disabled = true;
 	setTimeout(() => {
 		noteOff(device, context, note);
+		randomBtn.disabled = false;
 	}, 500);
 });
